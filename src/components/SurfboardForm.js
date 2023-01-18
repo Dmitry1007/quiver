e4ca32f209345b926f06578b7e7ca2e23bdb039e
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-18 11:06:58 -0700

    Wire up form to react-redux

--- src/components/SurfboardForm.js
+++ src/components/SurfboardForm.js
@@ -1,4 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
 function SurfboardForm() {
+  const dispatch = useDispatch();
+
+  const name = useSelector((state) => {
+    return state.form.name;
+  });
+
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+
   return (
     <div className="surfboard-form panel">
       <h4 className="subtitle is-3">Add Surfboard</h4>
@@ -8,8 +21,8 @@ function SurfboardForm() {
             <label className="label">Name</label>
             <input
               className="input is-expanded"
-              value={"name"}
-              onChange={() => {}}
+              value={name}
+              onChange={handleNameChange}
             />
           </div>
         </div>
