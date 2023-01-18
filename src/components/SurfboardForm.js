4ad84bed2ccd79d385f0bb4e4769948ab27ef6b6
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-18 10:38:53 -0700

    Boilerplate Surfboard form with name input field

--- src/components/SurfboardForm.js
+++ src/components/SurfboardForm.js
@@ -1,5 +1,21 @@
 function SurfboardForm() {
-  return <div>Surfboard Form</div>;
+  return (
+    <div className="surfboard-form panel">
+      <h4 className="subtitle is-3">Add Surfboard</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={"name"}
+              onChange={() => {}}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SurfboardForm;
