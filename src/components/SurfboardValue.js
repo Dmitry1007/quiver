05624eac32f702fdae3db1eb3286d56f85b2f262
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-19 17:08:59 -0700

    Much More JavaScripty way

--- src/components/SurfboardValue.js
+++ src/components/SurfboardValue.js
@@ -2,13 +2,11 @@ import { useSelector } from "react-redux";
 
 function SurfboardValue() {
   const totalCost = useSelector(({ surfboards: { data, searchTerm } }) => {
-    const filteredSurfboards = data.filter((surfboard) =>
-      surfboard.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    return filteredSurfboards.reduce(
-      (acc, surfboard) => acc + surfboard.cost,
-      0
-    );
+    return data
+      .filter((surfboard) =>
+        surfboard.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, surfboard) => acc + surfboard.cost, 0);
   });
 
   return <div className="surfboard-value">Total Cost: - ${totalCost}</div>;
